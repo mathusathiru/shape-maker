4c36d5f103358d31d711bbfddf4c1d1e05805989
Mathusa Thiruchenthoor <108091126+mathusathiru@users.noreply.github.com>
2024-06-15 22:59:06 +0100

    Append errors in inputs into the text area instead of the command line

--- InputHandler.java
+++ InputHandler.java
@@ -22,9 +22,9 @@ public class InputHandler {
 
         // Checks if the ID field is empty
         if (idText.equals("")) {
-            System.out.println("Error: ID is null; please enter a valid 6 digit integer ID value\n");
             JOptionPane.showMessageDialog(null, "Please enter a 6 digit ID value",
                     "Missing Value", JOptionPane.WARNING_MESSAGE);
+            theFrame.appendText("Error: ID is null; please enter a valid 6 digit integer ID value");
             return -1;
         }
 
@@ -33,11 +33,11 @@ public class InputHandler {
             if (idText.length() != 6) {
                 // Checks to see if the ID is a valid number
                 Integer.parseInt(idText);
-                System.out.println("Error: Invalid ID " + idText + " (too short at " + idText.length() +
-                        " characters); ID must be a 6 digit integer value e.g. 126658\n");
                 JOptionPane.showMessageDialog(null, "Invalid ID " + idText + ": ID is too short" +
                         " and must be a 6 digit number e.g. 126658", "Invalid ID", JOptionPane.ERROR_MESSAGE);
                 clearIDField();
+                theFrame.appendText("Error: Invalid ID " + idText + " (too short at " + idText.length() +
+                        " characters); ID must be a 6 digit integer value e.g. 126658");
                 return -1;
             }
 
@@ -46,10 +46,10 @@ public class InputHandler {
 
         } catch (NumberFormatException e) {
             // Handles cases where parsing to an int failed
-            System.out.println("Error: Invalid ID " + idText + " - ID must be a valid 6 digit number e.g. 126658\n");
             JOptionPane.showMessageDialog(null, "Invalid ID " + idText + " - ID must be a valid" +
                     " 6 digit number e.g. 126658", "Invalid ID", JOptionPane.ERROR_MESSAGE);
             clearIDField();
+            theFrame.appendText("Error: Invalid ID " + idText + " - ID must be a valid 6 digit number e.g. 126658");
             return -1;
         }
     }
@@ -62,10 +62,10 @@ public class InputHandler {
 
         // Check if sides field is empty
         if (sideText.equals("")) {
-            System.out.println("Error: number of sides is null; please enter a positive integer value for the number" +
-                    " of polygon sides e.g. 8\n");
             JOptionPane.showMessageDialog(null, "Please enter a whole number for the number " +
                             "of sides",  "Missing Value", JOptionPane.WARNING_MESSAGE);
+            theFrame.appendText("Error: number of sides is null; please enter a positive integer value for the number" +
+                    " of polygon sides e.g. 8");
             return -1;
         }
 
@@ -75,11 +75,11 @@ public class InputHandler {
 
             // Checks if the number of sides is negative
             if (sides < 0) {
-                System.out.println("Error: Invalid Side Number: " + sides + " - please enter a positive value for the" +
-                        " number of polygon sides e.g. 8\n");
                 JOptionPane.showMessageDialog(null, "Number of sides must be a positive whole" +
                                 " number", "Invalid Sides", JOptionPane.ERROR_MESSAGE);
                 clearSidesField();
+                theFrame.appendText("Error: Invalid Side Number: " + sides + " - please enter a positive value for the" +
+                        " number of polygon sides e.g. 8");
                 return -1;
             }
             // Displays an error if the number of sides is less than 3; invalid polygon
@@ -87,11 +87,11 @@ public class InputHandler {
                 JOptionPane.showMessageDialog(null, "Please enter a side number of three " +
                                 "or above for a valid polygon\n 0-2 sides will have an empty polygon or a " +
                                 "single straight line", "Invalid Sides", JOptionPane.ERROR_MESSAGE);
-                System.out.println("Error Invalid Side Number: " + sides + " - will not print a valid polygon; " +
-                        "the smallest possible number of sides for a valid polygon is three (for a triangle)," +
-                        "\nentering a value less than three will print an empty display (no sides or 1 side) or a " +
-                        "straight line (2 sides)\n");
                 clearSidesField();
+                theFrame.appendText("Error Invalid Side Number: " + sides + " - will not print a valid polygon; " +
+                        "the smallest possible number of sides for a valid polygon is three (for a triangle)," +
+                        "entering a value less than three will print an empty display (no sides or 1 side) or a " +
+                        "straight line (2 sides)");
                 return -1;
             }
 
@@ -100,11 +100,11 @@ public class InputHandler {
 
         } catch (NumberFormatException e) {
             // Handles cases where parsing to an int failed
-            System.out.println("Error: Invalid Side Number " + sideText + " - please enter a positive integer value " +
-                    "for the number of polygon sides, e.g. 8\n");
             JOptionPane.showMessageDialog(null, "Number of sides must be a whole integer number",
                     "Invalid Sides", JOptionPane.ERROR_MESSAGE);
             clearSidesField();
+            theFrame.appendText("Error: Invalid Side Number " + sideText + " - please enter a positive integer value " +
+                    "for the number of polygon sides, e.g. 8");
             return -1;
         }
     }
@@ -120,10 +120,10 @@ public class InputHandler {
 
         // Check if the angle field is empty.
         if (angleText.equals("")) {
-            System.out.println("Error: angle value is null; please enter an angle value such as 90, or 0 for no " +
-                    "angle rotation\n");
             JOptionPane.showMessageDialog(null, "Please enter an angle value",
                     "Missing Value", JOptionPane.WARNING_MESSAGE);
+            theFrame.appendText("Error: angle value is null; please enter an angle value such as 90, or 0 for no " +
+                    "angle rotation");
             return -1;
         }
 
@@ -132,12 +132,12 @@ public class InputHandler {
             return Double.parseDouble(angleText);
 
         } catch (NumberFormatException e) {
-            System.out.println("Error: Invalid Angle: " + angleText + " - please enter an angle value such " +
-                    "as 90, or 0 for no angle rotation\n");
             // Handle the case where parsing the angle as a double fails.
             JOptionPane.showMessageDialog(null, "Angle must be a valid number (can be decimal)",
                     "Invalid Angle", JOptionPane.ERROR_MESSAGE);
             clearAngleField();
+            theFrame.appendText("Error: Invalid Angle: " + angleText + " - please enter an angle value such " +
+                    "as 90, or 0 for no angle rotation");
             return -1;
         }
     }
@@ -151,9 +151,9 @@ public class InputHandler {
 
         // Checks if the radius field is empty
         if (radiusText.equals("")) {
-            System.out.println("Error: radius value is null; please enter a positive radius value such as 40\n");
             JOptionPane.showMessageDialog(null, "Please enter a radius value",
                     "Missing Value", JOptionPane.WARNING_MESSAGE);
+            theFrame.appendText("Error: radius value is null; please enter a positive radius value such as 40");
             return -1;
         }
 
@@ -163,31 +163,31 @@ public class InputHandler {
 
             // Checks if the radius is a negative number
             if (radius < 0) {
-                System.out.println("Error: Invalid Radius " + radius + " - please enter a positive radius value " +
-                        "such as 40\n");
                 JOptionPane.showMessageDialog(null, "Radius must be a positive number",
                         "Invalid Radius", JOptionPane.ERROR_MESSAGE);
                 clearRadiusField();
+                theFrame.appendText("Error: Invalid Radius " + radius + " - please enter a positive radius value " +
+                        "such as 40");
                 return -1;
             }
             // Displays an error if the radius is 0
             else if (radius == 0) {
-                System.out.println("Error: Invalid Radius 0 - polygon will not be displayed; please enter a positive" +
-                        " radius value above 0\n");
                 JOptionPane.showMessageDialog(null, "Radius set to 0; polygon will not be " +
                         "displayed", "Invalid Radius", JOptionPane.ERROR_MESSAGE);
                 clearRadiusField();
+                theFrame.appendText("Error: Invalid Radius 0 - polygon will not be displayed; please enter a positive" +
+                        " radius value above 0");
                 return -1;
             }
             return radius;
 
         } catch (NumberFormatException e) {
             // Handles cases where parsing the radius as a double failed
-            System.out.println("Error: Invalid Radius: " + radiusText + " - please enter a positive radius value " +
-                    "such as 40\n");
             JOptionPane.showMessageDialog(null, "Radius must be a valid number (can be decimal)",
                     "Invalid Radius", JOptionPane.ERROR_MESSAGE);
             clearRadiusField();
+            theFrame.appendText("Error: Invalid Radius: " + radiusText + " - please enter a positive radius value " +
+                    "such as 40");
             return -1;
         }
     }
@@ -198,21 +198,20 @@ public class InputHandler {
 
         // Checks if the searchID is empty
         if (searchID.equals("")) {
-            System.out.println("Error: Search ID is null; please enter a valid 6 digit integer Search ID value\n");
             JOptionPane.showMessageDialog(null, "Please enter a 6 digit ID value",
                     "Missing Value", JOptionPane.ERROR_MESSAGE);
+            theFrame.appendText("Error: Search ID is null; please enter a valid 6 digit integer Search ID value");
             return -1;
         }
 
         try {
             // Checks if searchID has exactly 6 digits
-            System.out.println("Error: Invalid Search ID " + searchID + " - ID must be a 6 digit integer value e.g. " +
-                    "126658\n");
             if (searchID.length() != 6) {
                 JOptionPane.showMessageDialog(null, "Invalid Search ID " + searchID + ": ID " +
                         "must be a 6 digit number such as 126658", "Invalid Search ID", JOptionPane.ERROR_MESSAGE);
-
                 clearIDField();
+                theFrame.appendText("Error: Invalid Search ID " + searchID + " - ID must be a 6 digit integer value e.g. " +
+                        "126658");
                 return -1;
             }
 
@@ -221,11 +220,11 @@ public class InputHandler {
 
         } catch (NumberFormatException e) {
             // Handles cases where parsing the searchID as an integer fails
-            System.out.println("Error: Invalid Search ID " + searchID + " - ID must be a valid 6 digit number e.g " +
-                    "126658\n");
             JOptionPane.showMessageDialog(null, "Invalid Search ID " + searchID + " - ID must " +
                     "be a valid 6 digit number e.g. 126658", "Invalid ID", JOptionPane.ERROR_MESSAGE);
             clearIDField();
+            theFrame.appendText("Error: Invalid Search ID " + searchID + " - ID must be a valid 6 digit number e.g " +
+                    "126658");
             return -1;
         }
     }
@@ -253,6 +252,5 @@ public class InputHandler {
         JTextField idField = theFrame.idField;
         idField.setText("");
     }
-
 
 }
